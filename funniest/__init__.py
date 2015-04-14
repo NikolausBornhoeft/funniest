21fcc1e4fd2bcfa08aab958a95968622abe08a83
NikolausBornhoeft <Klaus@neuschoe.de>
2015-04-14 15:39:30 +0200

    neu
    dddd

---
+++ funniest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 15:27:39 2015
+
+@author: bni
+"""
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 11:48:16 2015
+
+@author: bni
+"""
+
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/NikolausBornhoeft/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
